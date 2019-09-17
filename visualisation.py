026a75e3d336e6f922adcebe45a34dd2b92386f3
lame21 <44781213+lame21@users.noreply.github.com>
2019-09-17 12:28:21 +0200

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -4,3 +4,14 @@ data = np.loadtxt(
     fname='data/inflammation-01.csv',
     delimiter=','
 )
+ave_inflammation = np.mean(
+  data,
+     axis = 0
+) 
+plt.plot(ave_inflammation)
+
+ave_inflammation = np.max(
+  data,
+     axis = 0
+) 
+plt.plot(ave_inflammation)
